0581f124e2424f31e8c35203de824f8891cb670a
Rocío García Vijande <rociogvijande@gmail.com>
2023-03-20 16:33:40 +0100

    Incremental 11

---
+++ app.js
@@ -0,0 +1,30 @@
+var nombre;
+var apellido; 
+var anoNacimiento;
+var Cursos;
+
+nombre = prompt("Ingrese su nombre") ;
+apellido = prompt ("Ingrese su apellido") ;
+anoNacimiento = prompt ("Ingrese su año de nacimiento (en formato aaaa)") ;
+Cursos = prompt ("¿Cuántos cursos de programación has realizado?") ;
+
+// para que te lo escriba en la pantalla 
+let respuesta = `Hola mi nombre es ${nombre} mi apellido es ${apellido} nací en ${anoNacimiento} y he hecho ${Cursos} cursos de programación`
+document.write(respuesta)
+
+var suma; 
+var Cursos1 = parseInt (Cursos) 
+var number = 1
+suma = Cursos1 + 1 
+console.log( Cursos1 + number);
+
+
+// let anoNacimiento = parseInt(nacimiento)
+
+
+// if (anoNacimiento < 2000) {   alert("¿Estas seguro que eres un programador Junior? ")
+  
+//  } // 
+  
+// // 
+
